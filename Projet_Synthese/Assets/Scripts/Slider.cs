c99d860aa38a0ed3425b95be4754d29d698f97e8
Sl1mmy <noebousquet2004@gmail.com>
2024-05-15 17:22:34 -0400

    niveaux 1 2
    Co-Authored-By: Veryba <93687068+Veryba@users.noreply.github.com>

--- Projet_Synthese/Assets/Scripts/Slider.cs
+++ Projet_Synthese/Assets/Scripts/Slider.cs
@@ -5,16 +5,40 @@ using UnityEngine;
 public class Slider : MonoBehaviour
 {
     private RaymarchCam cam;
+    private float previousValue;
+    public float sensitivity = 0.5f;
+
     public UnityEngine.UI.Slider slider;
     // Start is called before the first frame update
     void Start()
     {
         cam = GetComponent<RaymarchCam>();
+
+        previousValue = slider.value;
+        slider.onValueChanged.AddListener(OnSliderValueChanged);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnSliderValueChanged(float newValue)
     {
+        // Calculate the change in value
+        float delta = newValue - previousValue;
+
+        // Adjust the value change speed
+        float adjustedDelta = delta * sensitivity;
+
+        // Remove the listener to prevent recursion
+        slider.onValueChanged.RemoveListener(OnSliderValueChanged);
+
+        // Update the slider value
+        slider.value = previousValue + adjustedDelta;
+
+        // Set the cam _wPosition
         cam._wPosition = slider.value;
+
+        // Re-add the listener
+        slider.onValueChanged.AddListener(OnSliderValueChanged);
+
+        // Update the previous value for the next iteration
+        previousValue = slider.value;
     }
 }
