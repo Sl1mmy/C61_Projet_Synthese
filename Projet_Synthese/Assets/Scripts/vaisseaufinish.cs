2fabd8f3fe03de332a09393ba1e030cbd35118fd
Veryba <maximegoaler@yahoo.com>
2024-05-22 21:38:49 -0400

    Level 5 et 6 fini

--- Projet_Synthese/Assets/Scripts/vaisseaufinish.cs
+++ Projet_Synthese/Assets/Scripts/vaisseaufinish.cs
@@ -15,16 +15,39 @@ public class vaisseaufinish : MonoBehaviour
 
     public LevelNames nextLevel;
     public int currentLevel = 0;
+    public float liftOffSpeed = 5f;
+    public float liftOffDuration = 2f;
+    public float waitBeforeLiftOff = 1f;
 
     private SaveSystem saveSystem = new SaveSystem();
+    private bool isLiftOff = false;
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") && !isLiftOff)
         {
+            isLiftOff = true;
             SaveGame(currentLevel);
-            SceneManager.LoadScene(nextLevel.ToString());
+            StartCoroutine(LiftOffAndLoadNextLevel(collision.gameObject));
         }
+    }
+
+    private IEnumerator LiftOffAndLoadNextLevel(GameObject player)
+    {
+
+        player.SetActive(false);
+        yield return new WaitForSeconds(waitBeforeLiftOff);
+        float elapsedTime = 0f;
+        Vector3 initialPosition = transform.position;
+
+        while (elapsedTime < liftOffDuration)
+        {
+            transform.position = initialPosition + Vector3.up * (liftOffSpeed * (elapsedTime / liftOffDuration));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        SceneManager.LoadScene(nextLevel.ToString());
     }
 
     /// <summary>
@@ -37,8 +60,9 @@ public class vaisseaufinish : MonoBehaviour
         string currentPlayerName = PlayerPrefs.GetString("CurrentPlayerName");
 
         PlayerData data = saveSystem.LoadGame(selectedSlot);
-        if (data.levelCompleted < currentLevel) { 
-            saveSystem.SaveGame(selectedSlot, currentPlayerName, currentLevel); 
+        if (data.levelCompleted < currentLevel)
+        {
+            saveSystem.SaveGame(selectedSlot, currentPlayerName, currentLevel);
         }
     }
 }
